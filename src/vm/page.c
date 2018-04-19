0c87b0b2cad5a401851eb0805f5efcee40e4fde4
Johnathan Becker <historybuffjb@gmail.com>
2018-04-18 19:16:35 -0600

    Merge branch 'johnb' into origin/johnb

--- src/vm/page.c
+++ src/vm/page.c
@@ -4,55 +4,61 @@
 #include "vm/frame.h"
 #include "threads/malloc.h"
 #include "threads/palloc.h"
-//=======p3
 #include <string.h>
 
 
 
 
-unsigned page_hash(const struct hash_elem *e, void *aux) {
+unsigned 
+page_hash(const struct hash_elem *e, void *aux) 
+{
     struct page *pg = hash_entry(e, struct page, hash_elem);
-    //change the return pull
-    //return ((int) pg->addr << PGBITS);
     return ((int) pg->addr >> PGBITS);
 }
 
-bool page_less(const struct hash_elem *a, const struct hash_elem *b, void* aux){
+bool 
+page_less(const struct hash_elem *a, const struct hash_elem *b, void* aux)
+{
     struct page *first = hash_entry(a, struct page, hash_elem);
     struct page *second = hash_entry(b, struct page, hash_elem);
-
     return (first->addr < second->addr);
 }
 
-//change
-//function to get a user page of memory
-//void *get_user_page(enum palloc_flags flags){
-// change function 
-// struct return -- still to get a user page of memory.
-//struct page *get_user_page(){
-// change to pulling based on our allocation, not a straight pull
-// Allocates a page of memory that contains the address void* addr.
-// Note: Doesn't allocate the struct frame yet. This is handled in page_in().
-struct page *page_allocate(void* addr){
+/* 
+change function to get a user page of memory
+void *get_user_page(enum palloc_flags flags){
+change function 
+struct return -- still to get a user page of memory.
+struct page *get_user_page(){
+change to pulling based on our allocation, not a straight pull
+Allocates a page of memory that contains the address void* addr.
+Note: Doesn't allocate the struct frame yet. This is handled in page_in(). 
+*/
+struct page*
+page_allocate(void* addr)
+{
     struct page *p = malloc(sizeof(struct page));
     struct thread *t = thread_current();
 
 
     p->addr = pg_round_down(addr);
     struct hash_elem *h_elem = hash_insert(t->page_table, &p->hash_elem);
-    // This page already exists in the table.
-    if(h_elem != NULL){
-        // free the existing structure.
+
+    /* This page already exists in the table. */
+    if(h_elem != NULL)
+    {
+        /* free the existing structure. */
         free(p);
-        // Return the structure that already exists in the table.
+        /* Return the structure that already exists in the table. */
         return hash_entry(h_elem, struct page, hash_elem);
-    }else{
-        // Return the newly allocated page.
+    }
+    else
+    {
+        /* Return the newly allocated page. */
         p->file = NULL;
         p->frame = NULL;
         p->sector = NULL;
-        return p;
-        
+        return p;        
     }
 }
 /*
@@ -65,22 +71,24 @@ memory is mapped to which processes’ virtual memory. This happens
 when you map a memory from physical to virtual.
 */
 //========================================================
-bool page_in(void *addr){
+bool 
+page_in(void *addr)
+{
     struct thread *t = thread_current();
 
-    // Temporary page structure.
+    /* Temporary page structure. */
     struct page p;
 
-    // Set p to the address we were given above.
+    /* Set p to the address we were given above. */
     p.addr = pg_round_down(addr);
     struct hash_elem *h_elem = hash_find(t->page_table, &p.hash_elem);
 
     if(!h_elem){
 
-        // Pointer to the new page.
+        /* Pointer to the new page. */
         struct page *pg = hash_entry(h_elem, struct page, hash_elem);
 
-        // Create a new zeroed page.
+        /* Create a new zeroed page. */
         if(pg->file == NULL && pg->sector == NULL && pg->frame == NULL){
             pg->frame = get_free_frame();
             lock_acquire(&pg->frame->f_lock);
@@ -90,6 +98,8 @@ bool page_in(void *addr){
     }
 }
 
-bool page_in_core(struct page *in_page){
+bool 
+page_in_core(struct page *in_page)
+{
     
 }
--- src/vm/page.h
+++ src/vm/page.h
@@ -7,8 +7,8 @@
 #include "filesys/file.h"
 #include "devices/block.h"
 
-// Structure to represent a virtual address page.
-struct page{
+struct page
+{
 
 void *addr;
 bool read_only;
@@ -26,33 +26,14 @@ struct file *file;
 off_t file_offset;
 off_t file_bytes;
 
-}; //end page
+};
 
-// Hash function for pages.
 unsigned page_hash(const struct hash_elem *e, void *aux);
 
-// Comparison function for pages.
 bool page_less(const struct hash_elem *a, const struct hash_elem *b, void* aux);
 
-//want a page func for 
-//void *get_user_page();
-//change function to return struct for our page
-//we want to get and possibly allocated a user page
-//struct page *get_user_page(void);
-//change, try returning based on the allocation instead.
-// Function to get (and possibly allocate) a user page.
 struct page *page_allocate(void* addr);
-/*
-USC Notes
-Virtual Address vs Physical Address
-Recall that virtual address is the processes’ addresses that they
-use. Physical memory is the actual memory in the hardware.
-You will have to do all the book-keeping to keep track of which physical
-memory is mapped to which processes’ virtual memory. This happens
-when you map a memory from physical to virtual.
-*/
 bool page_in(void *addr);
-
 bool page_in_core(struct page *in_page);
 
 #endif
